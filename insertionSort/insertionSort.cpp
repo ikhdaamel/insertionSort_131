7f3e6cd4a815b6d582af08999154c35f9cf97c36
ikhdaamel <ikhda.new45@gmail.com>
2023-03-23 15:27:19 +0700

    menampilkan di layar

--- insertionSort/insertionSort.cpp
+++ insertionSort/insertionSort.cpp
@@ -75,6 +75,12 @@ void display()
     cout << "\n=====================" << endl;      //output ke layar
     cout << "element array yang telah tersusun" << endl;    //output ke layar
     cout << "=================================" << endl;    //output ke layar
+
+    for (int j = 0; j < n; j++
+    {                                               //looping j dimulai dari 0 hingga n-1
+        cout << arr[j] << endl;                     //output ke layar
+
+    })
 }
 
 
